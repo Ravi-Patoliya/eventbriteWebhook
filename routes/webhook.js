a4a7e11f207d91c6e718faeb9824f6e9a491a44c
Ravi_009 <ravip@instaacoders.com>
2024-06-12 12:18:21 +0530

    initial commit

---
+++ routes/webhook.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const {
+//     webhook: { controller },
+// } = require('../controllers');
+
+// const router = express.Router();
+// router.get('/', controller.webhook);
+// // router.get('/avatars', controller.get);
+
+// module.exports = router;
